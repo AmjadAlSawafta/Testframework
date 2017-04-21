c3748099a185231137a70b3cca909fe44a55db76
Amjad Tubasi <eng.amjad.tubasi.at@gmail.com>
2017-04-21 03:13:22 +0300

    initial commit

---
+++ TestframeWork/TestFrameWork.swift
@@ -0,0 +1,16 @@
+//
+//  TestFrameWork.swift
+//  TestframeWork
+//
+//  Created by Amjad Private on 4/21/17.
+//  Copyright © 2017 Amjad Private. All rights reserved.
+//
+
+import UIKit
+
+public class TestFrameWork: NSObject {
+
+    public class func testAmjadFramework(){
+        print("Amjad framework works!!!")
+    }
+}
---
+++ TestframeWork/TestframeWork.h
@@ -0,0 +1,19 @@
+//
+//  TestframeWork.h
+//  TestframeWork
+//
+//  Created by Amjad Private on 4/21/17.
+//  Copyright © 2017 Amjad Private. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestframeWork.
+FOUNDATION_EXPORT double TestframeWorkVersionNumber;
+
+//! Project version string for TestframeWork.
+FOUNDATION_EXPORT const unsigned char TestframeWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestframeWork/PublicHeader.h>
+
+
